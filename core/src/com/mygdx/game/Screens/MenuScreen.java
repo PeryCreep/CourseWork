c394fabad1ce721dec01b0343da24dd5a6cbe1c0
PeryCreep <polyakov02.artem@bk.ru>
2022-03-27 09:01:13 +0500

    add MenuScreen texture

--- core/src/com/mygdx/game/Screens/MenuScreen.java
+++ core/src/com/mygdx/game/Screens/MenuScreen.java
@@ -1,30 +1,20 @@
 package com.mygdx.game.Screens;
 
-
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.mygdx.game.Actors.BaseActor;
-import com.mygdx.game.Actors.Hero;
-import com.mygdx.game.Actors.TiledActor;
 import com.mygdx.game.BaseGame;
-import org.graalvm.compiler.replacements.Log;
 
 public class MenuScreen extends BaseScreen {
-    private BaseActor mainMenuBackground;
 
 
     public MenuScreen(BaseGame game) {
         super(game);
-        mainMenuBackground = new BaseActor(0, 0, mainStage);
-
-        mainMenuBackground.setTexture("Screens/MenuScreen.png");
     }
 
     @Override
     public void initialize() {
-
+        BaseActor mainMenuBackground = new BaseActor(0, 0, mainStage);
+        mainMenuBackground.setTexture("Screens/MenuScreen.png");
     }
 
     @Override
