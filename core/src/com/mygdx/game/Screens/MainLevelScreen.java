5f82cd2b2a0c093071adfae6f020331e399502d6
PeryCreep <polyakov02.artem@bk.ru>
2022-06-01 10:42:39 +0500

    call update method in render

--- core/src/com/mygdx/game/Screens/MainLevelScreen.java
+++ core/src/com/mygdx/game/Screens/MainLevelScreen.java
@@ -106,6 +106,8 @@ public class MainLevelScreen extends BaseScreen {
                 mainStage.act();
                 uiStage.act();
 
+                update();
+
                 Gdx.gl.glClearColor(0, 0, 0, 1);
                 Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
